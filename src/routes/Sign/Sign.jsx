28ab6ea179ae0daf535493d1da8182efb22637b5
MichaelYuhe <2312744987@qq.com>
2022-05-09 20:22:28 +0800

    Button change

--- src/routes/Sign/Sign.jsx
+++ src/routes/Sign/Sign.jsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
-import { Button } from '@mui/material';
 import './index.css'
 
 export default function Sign() {
